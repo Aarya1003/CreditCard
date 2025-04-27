ba6bd683d45ce88459278fe306bd709f1f6c0dc0
Aarya1003 <143012264+Aarya1003@users.noreply.github.com>
2025-04-27 21:22:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pickle
 import numpy as np
 
-model=pickle.load(open(r"Desktop\Credit Card\vc.pkl","rb"))
+model=pickle.load(open(r"vc.pkl","rb"))
 
 # create Streamlit app
 st.title("Credit Card Fraud Detection Model")
